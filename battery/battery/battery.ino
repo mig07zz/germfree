f4d14c54c41dc66a85f005aad0033db3d65b540a
peyman <peyman@udel.edu>
2020-04-29 00:14:17 -0400

    added code for essp32 battery level and eeprom

---
+++ battery/battery/battery.ino
@@ -0,0 +1,36 @@
+#define VBATPIN A13
+#include <EEPROM.h>
+
+int pey;
+
+
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  Serial.begin(115200);
+  EEPROM.begin(1);
+  EEPROM.write(0, 50);
+  pey = EEPROM.read(0);
+
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2.0;    // we divided by 2, so multiply back
+
+  measuredvbat /= 1150.0; // convert to voltage
+
+  float percentage = (measuredvbat-3.2)*100L/(4.2-3.2);
+  Serial.print("VBat: " ); Serial.print(measuredvbat);Serial.print(" ,percentage: " ); Serial.println(percentage);
+
+  
+  Serial.println(pey);
+
+
+  
+  delay(2000);                       // wait for a second
+}
